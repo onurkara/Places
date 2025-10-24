abd3b9182cbbf494aed435d8c7ecdcc22c405a67
Onur Kara <onurkara92@gmail.com>
2025-10-24 21:38:00 +0200

    Create empty repo

---
+++ ABN-AMRO-iOS-Case/ABN-AMRO-iOS-Case/ABN_AMRO_iOS_CaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABN_AMRO_iOS_CaseApp.swift
+//  ABN-AMRO-iOS-Case
+//
+//  Created by Onur Kara on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ABN_AMRO_iOS_CaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
